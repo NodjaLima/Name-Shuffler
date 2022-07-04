d1b0243ed2e3ffbec8b94677bd1cbf558ed41b2a
NodjaLima <nodjamplima@gmail.com>
2022-07-04 11:09:18 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function nameShuffler(str){
+  const a = str.split(' ')
+  return `${a[1]} ${a[0]}`
+  
+}
+
+nameShuffler('john McClane')
+
+//return str.split(' ').reverse().join(' ') - reposta que apareceu nas soluções e eu achei interessante deixar aqui comentado para futuras consultas
